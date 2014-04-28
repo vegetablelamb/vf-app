2051a26ba23b65a13cd88b117228279f2f36ffaa
Alan Hunt <a.hunt.uk@googlemail.com>
2014-04-28 03:12:04 +0100

    added cookies for local answers

--- js/controllers/PollsController.js
+++ js/controllers/PollsController.js
@@ -1,96 +1,140 @@
 pollingApp.controller('PollsController', function($window, $rootScope, $scope, $state, ipCookie) {
-	$scope.polls = [];
-	$scope.answeredQuestions = {};
+    
+    $scope.polls                = [];
+    $scope.answeredQuestions    = {};
 
-	socket.emit('questionsRequest');
+    socket.emit('questionsRequest');
 
-	$scope.refresh = function() {
-		socket.emit('questionsRequest');
-		$scope.checkAnsweredQuestions();
-	}
+    $scope.generateCookie = function() {
+        if(!ipCookie('answeredPolls')){
+            ipCookie('answeredPolls', '{}');
+        }
+        if(!ipCookie('answered')){
+            ipCookie('answered', '{}');
+        }
+    }
 
-	$scope.checkAnsweredQuestions = function() {
-		var cookieData = ipCookie('answeredPolls');
+    $scope.refresh = function() {
+        socket.emit('questionsRequest');
+        $scope.checkAnsweredQuestions();
+    }
 
-		if (cookieData) {
-			$scope.answeredQuestions = cookieData;
-		}
-	}
+    $scope.checkAnsweredQuestions = function() {
+        var cookieData = ipCookie('answeredPolls');
+        if (cookieData) {
+            $scope.answeredQuestions = cookieData;
+        }
+    }
 
-	$scope.isPollAnswered = function(poll) {
-		return $scope.answeredQuestions[poll.id] ? true : false;
-	}
+    $scope.isPollAnswered = function(poll) {
+        return $scope.answeredQuestions[poll.id] ? true : false;
+    }
 
-	$scope.selectAnswer = function(poll, answer) {
-		if ($scope.answeredQuestions[poll.id]) {
-			return false;
-		}
-		answer.times++;
-		poll.question.times++;
-		answer.selected = true;
-		$scope.updateCookie(poll);
-		socket.emit('pollUpdate', poll);
-	}
+    $scope.selectAnswer = function(poll, answer) {
+        if ($scope.answeredQuestions[poll.id]) {
+            return false;
+        }
+        answer.times++;
+        poll.question.times++;
+        console.log(answer);
+        answer.selected = true;
+        $scope.updateAnswerCookie(poll, answer);
+        $scope.updateCookie(poll);
+        socket.emit('pollUpdate', poll);
+    }
 
-	$scope.updateCookie = function(poll) {
-		var cookie = ipCookie('answeredPolls');
-		
-		if (cookie && cookie.length > 0) {
-			cookie = JSON.parse(cookie);
-		}
-		else cookie = {};
+    $scope.updateAnswerCookie = function(poll, answer) {
+        var cookie = ipCookie('answered');
 
-		cookie[poll.id] = 'answered';
+        cookie[poll.id] = answer.text;
 
-		ipCookie('answeredPolls', JSON.stringify(cookie), {expires: 99});
-		$scope.refresh();
-	}
+        ipCookie('answered', JSON.stringify(cookie), {expires: 99});
 
-	$scope.submitQuestion = function(form) {
-		var answer;
+        $scope.refresh();
+        console.log('ipCookie.answered',ipCookie('answered'));
+    }
 
-		for (answer in form.answers) {
-			form.answers[answer].times = 0;
-		} 
+    $scope.updateCookie = function(poll) {
+        var cookie = ipCookie('answeredPolls');
+        // if (cookie && JSON.stringify(cookie) !== '{}') {
+        //     cookie = JSON.parse(JSON.stringify(cookie));
+        //     //cookie = JSON.parse(cookie);
+        // }else cookie = {};
 
-		socket.emit('newQuestion', {
-			'id': $scope.polls.length,
-			'question': {
-				'text': form.newQuestion,
-				'times': 0
-			},
-			'answers': form.answers
-		}); 
-	} 
 
-	$scope.$on('$destroy', function (event) {
+        cookie[poll.id] = 'answered';
+
+        ipCookie('answeredPolls', JSON.stringify(cookie), {expires: 99});
+        console.log('ipCookie',ipCookie('answeredPolls'));
+        $scope.refresh();
+        console.log('ipCookie',ipCookie('answeredPolls'));
+    }
+
+    $scope.submitQuestion = function(form) {
+        var answer;
+
+        for (answer in form.answers) {
+            form.answers[answer].times = 0;
+        } 
+
+        socket.emit('newQuestion', {
+            'id': $scope.polls.length,
+            'question': {
+                'text': form.newQuestion,
+                'times': 0
+            },
+            'answers': form.answers
+        }); 
+    } 
+
+    $scope.$on('$destroy', function (event) {
         socket.removeAllListeners();
     });
 
-	socket.on('pollUpdateSuccess', function(poll) {
-		$scope.refresh();
-	});
+    socket.on('pollUpdateSuccess', function(poll) {
+        $scope.refresh();
+    });
 
-	socket.on('newQuestionSaved', function(data) {
-		$scope.refresh();
-		$state.go('admin.viewquestions');
-	});
+    socket.on('newQuestionSaved', function(data) {
+        $scope.refresh();
+        $state.go('admin.viewquestions');
+    });
 
-	socket.on('questionsData', function(data) {
-		$scope.$apply(function() {
-			$scope.polls = [];
-			var poll;
+    socket.on('questionsData', function(data) {
+        $scope.$apply(function() {
+            $scope.polls = [];
+            var poll,
+                cookie = ipCookie('answered');
 
-			for (poll in data) {
-				if (data[poll]) {
-					if (data[poll].length > 0) {
-						data[poll] = JSON.parse(data[poll]);
-						$scope.polls.unshift(data[poll]);
-					}
-				}
-			}
-		});
-	});
+            for (poll in data) {
+                if (data[poll]) {
+                    if (data[poll].length > 0) {
+                        data[poll] = JSON.parse(data[poll]);
+                        // $scope.polls.unshift(data[poll]);
+                        console.log(data[poll].id);
+                        console.log(data[poll].answers);
+                        var qId = data[poll].id;
+                        for (var i = 0; i < data[poll].answers.length; i++) {
+                            console.log(qId, data[poll].answers[i].text);
+                            console.log(cookie);
+                            if(cookie[qId] === data[poll].answers[i].text){
+                                    console.info('FOUND', data[poll]);
+                                    data[poll].answers[i].selectedLocal = true;
+                                    console.info(data[poll].answers[i].selectedLocal);
+                            }
+                            // if(cookie[]){
 
-	$scope.checkAnsweredQuestions();
+                            // }
+                        }
+                        $scope.polls.unshift(data[poll]);
+
+                    }
+                }
+            }
+            console.log($scope.polls);
+        });
+    });
+
+    $scope.generateCookie();
+    $scope.checkAnsweredQuestions();
 });
